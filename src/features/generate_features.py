ded02b3dc9fc1d07f32718899ef3bbb800821686
ben efron <108874119+benefoon@users.noreply.github.com>
2025-04-09 00:47:25 +0330

    Update generate_features.py

--- src/features/generate_features.py
+++ src/features/generate_features.py
@@ -2,11 +2,9 @@ import pandas as pd
 
 def generate_basic_features(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Generates basic features such as name length and foreign transaction flag.
+    Generate simple numerical features from transaction data.
     """
     df = df.copy()
-
-    df['name_length'] = df['name'].apply(lambda x: len(str(x)))
-    df['is_foreign'] = df['country'].apply(lambda x: 1 if x != 'US' else 0)
-
+    df["name_length"] = df["name"].apply(lambda x: len(x))
+    df["is_foreign"] = df["country"].apply(lambda c: int(c != "US"))
     return df
